ddb68029072605bf5c41a4e87511cb9542c72dac
Andres Lopez <andres_lopez_rm@hotmail.com>
2024-08-27 11:16:46 -0600

    Add files via upload

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,25 @@
 import { Injectable } from '@nestjs/common';
+const fs = require('file-system');
+
 
 @Injectable()
 export class AppService {
   getHello() {
-    return { status: 'ok' };
+    const fileContent = fs.readFileSync('./src/files/postal_code.json');
+    const data = JSON.parse(fileContent);
+    const response = data.filter((entry:any )=> entry.postal_code === '01040');
+    if (response.length >= 1) {
+      return response.map((element: any) => {
+        return {
+          postal_code: element.postal_code,
+          colony: element.colony,
+          type: element.type,
+          alc_o_mun: element.a_o_m,
+          state: element.state,
+          city: element.city
+        };
+      });
+    }
+    return response;
   }
 }
